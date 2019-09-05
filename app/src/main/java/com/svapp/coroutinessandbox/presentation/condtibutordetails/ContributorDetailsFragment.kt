7e07c27d4aee953f42c398bdaff581b513f67850
Valentyn Smetanin <smetaninvalentyn@gmail.com>
2019-09-05 18:52:25 +0300

    Added args of by arch components

--- app/src/main/java/com/svapp/coroutinessandbox/presentation/condtibutordetails/ContributorDetailsFragment.kt
+++ app/src/main/java/com/svapp/coroutinessandbox/presentation/condtibutordetails/ContributorDetailsFragment.kt
@@ -1,6 +1,6 @@
 package com.svapp.coroutinessandbox.presentation.condtibutordetails
 
-import android.os.Bundle
+import androidx.navigation.fragment.navArgs
 import com.svapp.coroutinessandbox.R
 import com.svapp.coroutinessandbox.databinding.FragmentContributorDetailsBinding
 import com.svapp.coroutinessandbox.presentation.base.BaseFragment
@@ -12,28 +12,20 @@ import org.koin.android.viewmodel.ext.android.viewModel
 class ContributorDetailsFragment : BaseFragment<FragmentContributorDetailsBinding>() {
 
     private val mViewModel: ContributorDetailsViewModel by viewModel()
-
-    private var mLogin: String? = null
+    private val args: ContributorDetailsFragmentArgs by navArgs()
 
     override fun getLayoutResId() = R.layout.fragment_contributor_details
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            mLogin = it.getString("argLogin")
-        }
-    }
-
     override fun setupViews() {
         with(binding) {
-            lifecycleOwner = this@ContributorDetailsFragment
+            lifecycleOwner = this@ContributorDetailsFragment.viewLifecycleOwner
             viewModel = mViewModel
         }
         startViewModel()
     }
 
     private fun startViewModel() {
-        mLogin?.let {
+        args.argLogin.let {
             mViewModel.start(it)
         }
     }
