d0e6c4906b39cde530a69276bdabbc8842e01f97
Valentyn Smetanin <smetaninvalentyn@gmail.com>
2019-09-21 12:01:45 +0300

    Switch Glide to Coil

--- app/src/main/java/com/svapp/coroutinessandbox/presentation/base/BaseBindingAdapter.kt
+++ app/src/main/java/com/svapp/coroutinessandbox/presentation/base/BaseBindingAdapter.kt
@@ -2,18 +2,11 @@ package com.svapp.coroutinessandbox.presentation.base
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.load.engine.DiskCacheStrategy
+import coil.api.load
 
 /**
  * Created by Valentyn on 13.01.2019.
-*/
+ */
 
 @BindingAdapter("imageUrl")
-fun ImageView.loadImage(url: String?) {
-    url?.let {
-        GlideApp.with(this.context)
-            .load(it)
-            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-            .into(this)
-    }
-}
+fun ImageView.loadImage(url: String?) = url?.let { load(it) }
